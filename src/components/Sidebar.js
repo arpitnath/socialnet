a6bae592d4fc6e8309fdd73fba3264d1ec3232c8
arpitnath <arpitnath42@gmail.com>
2021-01-14 20:18:25 +0530

    sidebar and styling

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,14 +1,56 @@
 import { Avatar } from '@material-ui/core';
 import React from 'react';
-import { SidebarComp, BgImg, SidebarTitle, SidebarSpan } from './styled';
+import {
+	SidebarComp,
+	BgImg,
+	SidebarTitle,
+	SidebarSpan,
+	SidebarTop,
+	SidebarStats,
+	Stats,
+	StatNum,
+	SidebarBottom,
+	Paragraph,
+	Recent,
+	Span,
+	RecentP,
+} from './styled';
 
 const Sidebar = () => {
+	const recentItems = (topic) => (
+		<Recent>
+			<Span>#</Span>
+			<RecentP>{topic}</RecentP>
+		</Recent>
+	);
+
 	return (
 		<SidebarComp>
-			<BgImg />
-			<Avatar />
-			<SidebarTitle>Sample Name</SidebarTitle>
-			<SidebarSpan>test@gmail.com</SidebarSpan>
+			<SidebarTop>
+				<BgImg src='https://images.unsplash.com/photo-1579546929518-9e396f3cc809?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=2550&q=80' />
+				<Avatar className='sidebarAvatar' />
+
+				<SidebarTitle>Sample Name</SidebarTitle>
+				<SidebarSpan>test@gmail.com</SidebarSpan>
+			</SidebarTop>
+			<SidebarStats>
+				<Stats>
+					<Paragraph>Profile views</Paragraph>
+					<StatNum>234</StatNum>
+				</Stats>
+				<Stats>
+					<Paragraph>Views on post</Paragraph>
+					<StatNum>34</StatNum>
+				</Stats>
+			</SidebarStats>
+			<SidebarBottom>
+				<p>Recent</p>
+				{recentItems('ReactJs')}
+				{recentItems('NodeJs')}
+				{recentItems('ExpressJs')}
+				{recentItems('Python')}
+				{recentItems('babel')}
+			</SidebarBottom>
 		</SidebarComp>
 	);
 };
--- src/components/styled.js
+++ src/components/styled.js
@@ -69,9 +69,94 @@ export const TitleH3 = styled.h3`
  */
 
 export const SidebarComp = styled.div`
+	position: sticky;
+	top: 80px;
+	flex: 0.2;
+	border-radius: 10px;
+	text-align: center;
+	height: fit-content;
 	color: #f5f5f5;
 `;
 
-export const BgImg = styled.img``;
-export const SidebarTitle = styled.h2``;
-export const SidebarSpan = styled.h4``;
+export const SidebarTop = styled.div`
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	border: 1px solid #616161;
+	border-bottom: 0;
+	border-top-left-radius: 10px;
+	border-top-right-radius: 10px;
+	background-color: #1a1c2f;
+	padding-bottom: 10px;
+`;
+
+export const SidebarBottom = styled.div`
+	text-align: left;
+	padding: 10px;
+	border: 1px solid #616161;
+	border-radius: 5px;
+	margin-top: 10px;
+	background-color: #1a1c2f;
+	font-weight: bold;
+	font-size: 15px;
+`;
+export const SidebarStats = styled.div`
+	display: flex;
+	justify-content: space-between;
+	padding: 10px;
+	margin-bottom: 10px;
+	background-color: #1a1c2f;
+	border: 1px solid #616161;
+`;
+
+export const Paragraph = styled.p`
+	color: #e0e0e0;
+	font-size: 12px;
+	font-weight: bold;
+	opacity: 0.5;
+`;
+export const Stats = styled.div`
+	margin-top: 10px;
+`;
+export const StatNum = styled.div`
+	font-weight: bold;
+	color: #85c5e5;
+`;
+
+export const BgImg = styled.img`
+	margin-bottom: -20px;
+	width: 100%;
+	height: 80px;
+	border-top-left-radius: 5px;
+	border-top-right-radius: 5px;
+	object-fit: cover;
+`;
+export const SidebarTitle = styled.h2`
+	font-size: 18px;
+	color: #fafafa;
+`;
+export const SidebarSpan = styled.h4`
+	font-size: 12px;
+	color: #e0e0e0;
+`;
+
+export const Recent = styled.div`
+	display: flex;
+	font-size: 13px;
+	color: #bdbdbd;
+	font-weight: bold;
+	cursor: pointer;
+	padding: 5px;
+
+	&:hover {
+		background-color: whitesmoke;
+		border-radius: 5px;
+		cursor: pointer;
+		color: black;
+	}
+`;
+export const Span = styled.span`
+	margin-right: 10px;
+	margin-left: 5px;
+`;
+export const RecentP = styled.p``;
